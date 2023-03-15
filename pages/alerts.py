09cb8e56487d766d76fc816f1f25b9afccc816b8
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-03-15 15:31:06 +0300

    alerts_
    Alert tests

--- pages/alerts.py
+++ pages/alerts.py
@@ -1,14 +1,19 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class AlertsPage():
     def __init__(self, driver):
         self.driver = driver
     
+    ALERT_BUTTON=(By.ID,"alertButton")
+    CONFİRM_BUTTON=(By.XPATH,"//*[@id='confirmButton']")
+    CONFİRM_RESULT=(By.XPATH,"//*[@id='confirmResult']")
+    PROMPT_RESULTS=(By.XPATH,"//*[@id='promptResult']")
+    PROMPT_BUTTON=(By.XPATH,"//*[@id='promtButton']")
     def click_js_alert_button(self):
-        self.driver.find_element(By.ID,"alertButton").click()
+        self.driver.find_element(*AlertsPage.ALERT_BUTTON).click()
     
-    def catch_alert(self):
-         
+    def catch_alert(self):  
         alert=self.driver.switch_to.alert
         return alert.text
     
@@ -17,10 +22,36 @@ class AlertsPage():
         alert.accept()
         
     def click_js_confirm_button_to_running_alert(self):
-        self.driver.find_element(By.XPATH,"//*[@id='confirmButton']").click()
+        self.driver.find_element(*AlertsPage.CONFİRM_BUTTON).click()
         
     def alert_accept(self):
         self.driver.switch_to.alert.accept()
         
     def alert_confirm_text(self):
-        return self.driver.find_element(By.XPATH,"//*[@id='confirmResult']").text
+        return self.driver.find_element(*AlertsPage.CONFİRM_RESULT).text
+    
+    def alert_not_confirm(self):
+        self.driver.switch_to.alert.dismiss()
+    
+    def alert_send_key(self,key):
+        self.driver.switch_to.alert.send_keys(key)
+        
+    def alert_send_prompt_and_accept_then_get_key(self,key):
+        self.driver.switch_to.alert.send_keys(key)
+        self.driver.switch_to.alert.accept()
+        return self.driver.find_element(*AlertsPage.PROMPT_RESULTS).text
+        
+    def click_js_prompt_button(self):
+        self.driver.find_element(*AlertsPage.PROMPT_BUTTON).click()
+        
+    def alert_promt_visibility(self):
+        wait = WebDriverWait(self.driver, 1)
+        try:
+            wait.until(EC.visibility_of_element_located((AlertsPage.PROMPT_RESULTS)))
+            result = True
+        except:
+            result = False
+        return result
+         
+    
+    
